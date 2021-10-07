538271dced2668fa3b5cc1bf5a8df7eebc96afe0
IlyesDjari <ilyes.djari@student.ehb.be>
2021-10-07 15:40:24 +0200

    Hello world

---
+++ Les 1/src/main/kotlin/helloworld.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //println("Hello World")
+helloYou()
+}
+
+fun helloYou() {
+    val helloFriends = arrayOf("Benoit","Finn","Freya","Ilyes","Jeroen")
+    val hello = "hello"
+    val empty = " "
+    for (names in helloFriends)
+    println(hello+empty+names)
+}
